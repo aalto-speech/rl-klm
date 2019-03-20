40479085abc4ecf6ffd9c1561230cd755c80b953
kkleino <katri.k.leino@aalto.fi>
2019-03-20 12:58:43 -0700

    cleaning

--- demo/rl-klm_demo/initialParams.py
+++ demo/rl-klm_demo/initialParams.py
@@ -13,21 +13,16 @@ class initializeParams:
         self.num_states = np.size(x_coord)
         self.num_of_mods = 1
 
-        self.start = 1
-        self.end = 1 
-        self.random_search = False
-        self.random_search_iters = 5
-
         # KLM estiamtes depend on the distance of previous and current item.
-        self.penalties = np.array([2]) # distance between items
-        self.widths = np.array([40]*self.num_states) # Width of the item
-        self.dimensions = [200, 40] # Width, Height
+        self.penalties = np.array([2]) # Not used in demo
+        self.widths = np.array([40]*self.num_states) # Width of the items
         self.init_position = [x_coord[0], y_coord[0]]
         self.goal_position = [x_coord[self.num_states-1], y_coord[self.num_states-1]]
         self.grid = [x_coord[1:self.num_states], y_coord[1:self.num_states]] # 
-        #self.grid = [[0, 0., 6.5, 13., 0., 6.5, 13., 0., 6.5, 13.],[0, 0., 0., 0., 2., 2., 2., 4., 4. ,4.]] # X, Y
-        self.fitts_a = 54.38 #1033 #54.38
-        self.fitts_b = 14.62 #96 #14.62
+
+        # Fitts Law parameters
+        self.fitts_a = 54.38 
+        self.fitts_b = 14.62 
 
 
         # Sensor errors
@@ -40,10 +35,12 @@ class initializeParams:
         # Initial penalty for moving hand to control the remote
         self.mod_penalty = 0
 
-        # The number of best UIs that will be saved into text file after the optimization
-        self.top = 1
-        self.the_top = 1
 
+        ## Optimization parameters
+        self.start = 1
+        self.end = 1 
+        self.random_search = False
+        self.random_search_iters = 5
         # Objective function weights
         self.w_klm = 1 # For KLM
         self.w_const = 1 # For learnability / consistency
@@ -52,13 +49,10 @@ class initializeParams:
         self.multi_objective = True
         self.objectives = [[1,1,0],[0,1,1],[1,1,1]] #multi-objective
         
+        # The number of best UIs that will be saved into text file after the optimization
+        self.top = 1
+        self.the_top = 1
 
 
 
-
-    # 
-    def setSensorErrors(self, e_t, e_g, e_s):
-        self.sensor_errors[0] = float(e_t)
-        self.sensor_errors[1] = float(e_g)
-        self.sensor_errors[2] = float(e_s)
 
